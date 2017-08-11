471284bc089a0f43cbff8d41baf50fe68dbe5eb0
Harvey Multani <harvey.multani@gmail.com>
2017-08-11 02:16:13 -0700

    Fix body values

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ var Expense = require('./db/models/expense');
 var app = express();
 
 app.all('*', function(req, res, next) {
-     var origin = req.get('origin');
-     res.header('Access-Control-Allow-Origin', origin);
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     res.header('Access-Control-Allow-Headers', 'Content-Type');
-     next();
-});
+    var origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(morgan('combined'));
@@ -34,15 +34,20 @@ app.get('/expenses', function(req, res) {
 });
 
 app.post('/expenses', function(req, res) {
+  // console.log(req.body.data);
+  var date = req.body.data.date;
+  var merchant = req.body.data.merchant;
+  var amount = req.body.data.amount;
+
   var expense = new Expense({
-    date: req.body.date,
-    merchant: req.body.merchant,
-    amount: req.body.amount
-  })
+    date: date,
+    merchant: merchant,
+    amount: amount
+  });
 
   expense.save(function(err, save) {
     if (err) {
-      return next(err);
+      res.send('this is an error');
     }
     res.send(`${expense.date}, ${expense.merchant}, ${expense.amount}`);
   });
